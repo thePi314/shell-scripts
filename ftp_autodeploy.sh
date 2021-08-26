01d744154d65aeadc24068d18993b3b75ad9cb00
Plasto Imran <48218528+thePi314@users.noreply.github.com>
2021-08-26 17:06:09 +0200

    FTP Auto deploy script
    Scripts automates project upload trough ftp protocol. So many hosting services still uses old system of packaging services which they provide. In my instance i was not able to use ssh service in basic package which I consider a bullshit. You would be force to upload everything step by step so i went step forward and made this little script to automate that process. Feel free to modify it since this is only a basic need :)

---
+++ ftp_autodeploy.sh
@@ -0,0 +1,80 @@
+#!/bin/bash
+
+# default project root
+arg_directory="./"
+
+
+while test $# -gt 0; do
+    case "$1" in
+        --host)
+            shift
+            arg_host=$1
+            shift
+            ;;
+        --username)
+            shift
+            arg_username=$1
+            shift
+            ;;
+        --password)
+            shift
+            arg_password=$1
+            shift
+            ;;
+        --help)
+            echo ""
+            echo "Autodeployer shell script tool"
+            echo "        -- by mrpi --"
+            echo ""
+            echo "--host:      Define hostname for ftp connection"
+            echo "--username:  Define ftp username"
+            echo "--password:  Define passwor for specified ftp username"
+            echo "--directory: Define project root, default is current directory"
+            echo ""
+            exit 1;
+            ;;
+        --directory)
+            shift
+            arg_directory=$1
+            shift
+            ;;
+        *)
+            echo "$1 is not a recognized flag!"
+            exit 1;
+            ;;
+    esac
+done  
+
+
+exec_ftp_script="
+quote USER $arg_username
+quote PASS $arg_password
+cd public_html
+"
+
+
+scanner(){
+    for entry in "$search_dir"$1
+    do
+        dir=${entry:2}
+        if [ -d "$entry" ]; then
+            exec_ftp_script=$exec_ftp_script"mkdir ${dir/#${2:2}}
+            "
+            
+            scanner "./"${entry:2}"/*" $2
+        fi
+
+        if [ -f "$entry" ]; then
+            exec_ftp_script=$exec_ftp_script"put $2${dir/#${2:2}} ${dir/#${2:2}}
+            "
+        fi
+    done    
+}
+
+
+scanner $arg_directory"*" $arg_directory
+
+
+ftp -p -nv $arg_host << END_SCRIPT
+$exec_ftp_script
+END_SCRIPT
